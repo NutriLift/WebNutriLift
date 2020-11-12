88c05157c75e5d8b23cc53f364073e8db1fca012
Alan Ekins <alan@nutrilift.co.uk>
2020-11-12 06:45:58 +0000

    DB Name Update

--- NuteriLift_API/Entities/UserDetails.cs
+++ NuteriLift_API/Entities/UserDetails.cs
@@ -4,17 +4,17 @@ namespace NuteriLift_API.Entities
     public class UserDetails
     {
 
-        public Guid UserId { get; set; }
-        public bool IsActive { get; set; }
-        public string FirstName { get; set; }
-        public string FamilyName { get; set; }
-        public string UserName { get; set; }
-        public byte[] UserPassword { get; set; }
-        public byte[] PasswordSalt { get; set; }
-        public DateTime BirthDate { get; set; }
-        public char Gender { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public bool IsAdmin { get; set; }
+        public Guid UD_PK { get; set; }
+        public bool UD_IsActive { get; set; }
+        public string UD_FirstName { get; set; }
+        public string UD_FamilyName { get; set; }
+        public string UD_UserName { get; set; }
+        public byte[] UD_UserPassword { get; set; }
+        public byte[] UD_PasswordSalt { get; set; }
+        public DateTime UD_BirthDate { get; set; }
+        public char UD_Gender { get; set; }
+        public DateTime UD_CreatedDate { get; set; }
+        public bool UD_IsAdmin { get; set; }
 
         public UserDetails()
         {
